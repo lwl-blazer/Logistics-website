458268b3412efef8a7636e3a125475daf0d842da
louwl <215208364@qq.com>
2017-06-15 14:27:40 +0800

    add slick

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,11 @@
 /**
  * Created by blazer on 2017/6/7.
  */
+
+$(document).ready(function () {
+    $("#banner-image").slick({
+        autoplay: true,
+        arrows:false,
+        dots:true
+    });
+});
